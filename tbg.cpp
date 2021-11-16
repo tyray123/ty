5b8d93ea3a2ee2d5a6ad5904cd272fee902acc7d
tyray123 <94390292+tyray123@users.noreply.github.com>
2021-11-16 09:59:46 -0700

    Update tbg.cpp

--- tbg.cpp
+++ tbg.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 using namespace std;
+string inventory[10];
+void shop();
+int money = 500;
 int main() {
 	// game variables
 	int room = 1;
 	char input;
-
+	
 	cout << "you wake up to find yourself in a spoopy dungeon.can you escape?good luck!" << endl;
 
 	do { //beginning of the game loop---------------------------------------------------
+		
+		
 		switch (room) {
 		case 1:
+			shop();
 			cout << "you are in the room 1. you can go north." << endl;
+			
 			cin >> input;
 			if (input == 'n')
 				room = 2;
@@ -28,4 +35,29 @@ int main() {
 		}
 
 	} while (input != 'q');
+} //function defintions------------------------
+void shop() {
+	// remember to add "string inventory[10]"to your variables up top!
+	string input; // local to shop
+	do {
+		cout << "Hi,welcome to my shop!" << endl;
+		cout << "Enter k for key,p for potion,1 for lamp" << endl;
+		cout << "money:" << money << endl;
+		cout << "q to quit" << endl;
+		cin >> input;
+		if (input == "k") {
+			inventory[0] = "key";
+			cout << "you got a key!" << endl;
+			money -= 50;
+		}
+		else if (input == "p") {
+			inventory[1] = "potion";
+			cout << "you got a potoin!" << endl;
+			money -= 100;
+		}
+		else if (input == "1") {
+			inventory[2] = "lamp!";
+			cout << "you got a lamp!" << endl;
+		}
+	} while (input != "q");
 }
