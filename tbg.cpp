d315843bd4647012025c1cbe9eddc00e14f392a8
tyray123 <94390292+tyray123@users.noreply.github.com>
2021-12-03 09:21:59 -0700

    Update tbg.cpp

--- tbg.cpp
+++ tbg.cpp
@@ -1,181 +1,98 @@
 #include<iostream>
 using namespace std;
 string inventory[10];
-void shop();
+void pay();
 int money = 500;
+int time = 1190;
 int main() {
 	// game variables
 	int room = 1;
 	char input;
 	
-	cout << "you wake up to find yourself in a spoopy dungeon.can you escape?good luck!" << endl;
+	cout << "Hey,welcome to my skateboard shop! Your objective is to collect all the skateboard pieces and build the final one at the end" << endl;
 
 	do { //beginning of the game loop---------------------------------------------------
-		
-		
+		time += 10;
+		cout << time << endl;
+
 		switch (room) {
 		case 1:
-			shop();
-			cout << "you are in the room 1. you can go north." << endl;
-			
+			cout << " you've entered the skateboard shop. You can go north to collect the board and the grip. You can go east collect the bearings. You can go west to collect your wheels/chucks and complete your board." << endl;
 			cin >> input;
 			if (input == 'n')
 				room = 2;
+			else if (input == 'e')
+				room = 3;
+			else if (input == 'w')
+				room = 4;
+		
 			else
 				cout << "sorry, not an option." << endl;
 			break;
 		case 2:
-			cout << "you are in the room 2. you can go south." << endl;
+			cout << "   ." << endl;
 			cin >> input;
 			if (input == 's ')
 				room = 1;
 			else
 				cout << "sorry, not an option." << endl;
 			break;
-		}
+		case 3:
+			cout << "you are in the room 3. you can go west." << endl;
+			cin >> input;
+			if (input == 'w ')
+				room = 3;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
 
+
+		case 4:
+			cout << "you are in the room 4. you can go east." << endl;
+			cin >> input;
+			if (input == 'e ')
+				room = 4;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+
+
+
+		case 5:
+			cout << "when you enter room 5 ." << endl;
+			cin >> input;
+			if (input == 'e ')
+				room = 5;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+
+
+		case 6:
+			cout << "you are in the room 6. you can go east." << endl;
+			cin >> input;
+			if (input == 'e ')
+				room = 6;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		}
 	} while (input != 'q');
+			 
 } //function defintions------------------------
-void shop() {
+void pay() {
 	// remember to add "string inventory[10]"to your variables up top!
 	string input; // local to shop
 	do {
-		cout << "Hi,welcome to my shop!" << endl;
-		cout << "Enter k for key,p for potion,1 for lamp" << endl;
+		cout << "Hi great job making a skateboard!" << endl;
+		cout << "Enter p to pay. A skateboard is $500" << endl;
 		cout << "money:" << money << endl;
-		cout << "q to quit" << endl;
 		cin >> input;
-		if (input == "k") {
-			inventory[0] = "key";
-			cout << "you got a key!" << endl;
-			money -= 50;
-		}
-		else if (input == "p") {
-			inventory[1] = "potion";
-			cout << "you got a potoin!" << endl;
-			money -= 100;
-		}
-		else if (input == "1") {
-			inventory[2] = "lamp!";
-			cout << "you got a lamp!" << endl;
+		if (input == "p") {
+			inventory[0] = "skateboard";
+			cout << "you bought your skateboard!" << endl;
+			money -= 500;
+		
 		}
 	} while (input != "q");
 }
-#include<iostream>
-using namespace std;
-string inventory[10];
-void shop();
-int money = 500;
-int main() {
-	// game variables
-	int room = 1;
-	char input;
-
-	cout << "you wake up to find yourself in a spoopy dungeon.can you escape?good luck!" << endl;
-
-	do { //beginning of the game loop---------------------------------------------------
-
-
-		switch (room) {
-		case 1:
-			shop();
-			cout << "you are in the room 1. you can go north." << endl;
-
-			cin >> input;
-			if (input == 'n')
-				room = 2;
-			else
-				cout << "sorry, not an option." << endl;
-			break;
-		case 2:
-			cout << "you are in the room 2. you can go south." << endl;
-			cin >> input;
-			if (input == 's ')
-				room = 1;
-			else
-				cout << "sorry, not an option." << endl;
-			break;
-		}
-
-	} while (input != 'q');
-} //function defintions------------------------
-void shop() {
-	// remember to add "string inventory[10]"to your variables up top!
-	string input; // local to shop
-	do {
-		cout << "Hi,welcome to my shop!" << endl;
-		cout << "Enter k for key,p for potion,1 for lamp" << endl;
-		cout << "money:" << money << endl;
-		cout << "q to quit" << endl;
-		cin >> input;
-		if (input == "k") {
-			inventory[0] = "key";
-			cout << "you got a key!" << endl;
-			money -= 50;
-		}
-		else if (input == "p") {
-			inventory[1] = "potion";
-			cout << "you got a potoin!" << endl;
-			money -= 100;
-		}
-		else if (input == "1") {
-			inventory[2] = "lamp!";
-			cout << "you got a lamp!" << endl;
-		}
-	} while (input != "q");
-}    
-//Battle system defintion 
-		void Battle (int MonsterHealth) {
-			int damage;
-			while (health > 0 && MonsterHealth > 0) {
-				//the monster attacks
-				damage = rand() % 20;
-				cout << "the monster hurts you for " << damage << "damage." << endl;
-				health -= damage;//take the damage
-				//the human attacks 
-				if (inventory[4] == "sword") {
-					damage = rand() % 60 + 20;
-					cout << " you use your sword against the enmey" << endl;
-				}
-				else {
-					damage = rand() % 50 + 10;
-					cout << "you hit the monster with your fist" << endl;
-				}
-
-				cout << " you attack the monster for " << damage << "damage" << endl;
-				MonsterHealth -= damage;
-
-
-				}
-				if (MonsterHealth <= 0)
-					cout << "you defeated the monster!" << endl << endl;
-				else cout << "you died." << endl << endl;
-
-
-
-			}
-			
-		void npc(int ty) { 
-
-			string input;
-
-			while (input.compare("leave") != 0) {
-				switch (ty) {
-
-
-				case 1:"npc start talking to ty ";
-
-
-
-
-				}
-				case2:"  npc  ";
-
-
-
-
-			}
-
-
-		}
-		
