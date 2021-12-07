fbbee2b873c6f9720100aa53c2c8f6a59822456f
tyray123 <94390292+tyray123@users.noreply.github.com>
2021-12-07 09:27:29 -0700

    Update tbg.cpp

--- tbg.cpp
+++ tbg.cpp
@@ -30,17 +30,17 @@ int main() {
 				cout << "sorry, not an option." << endl;
 			break;
 		case 2:
-			cout << "   ." << endl;
+			cout << " your going north to get your board and grip  ." << endl;
 			cin >> input;
-			if (input == 's ')
+			if (input == 's')
 				room = 1;
 			else
 				cout << "sorry, not an option." << endl;
 			break;
 		case 3:
-			cout << "you are in the room 3. you can go west." << endl;
+			cout << "if you go west you can get your wheels and chucks." << endl;
 			cin >> input;
-			if (input == 'w ')
+			if (input == 'w')
 				room = 3;
 			else
 				cout << "sorry, not an option." << endl;
@@ -48,9 +48,9 @@ int main() {
 
 
 		case 4:
-			cout << "you are in the room 4. you can go east." << endl;
+			cout << "if you go east you can get the beanring ." << endl;
 			cin >> input;
-			if (input == 'e ')
+			if (input == 'e')
 				room = 4;
 			else
 				cout << "sorry, not an option." << endl;
@@ -59,7 +59,7 @@ int main() {
 
 
 		case 5:
-			cout << "when you enter room 5 ." << endl;
+			cout << " now you have 7 min to have every pieces before the timer runs out ." << endl;
 			cin >> input;
 			if (input == 'e ')
 				room = 5;
@@ -69,14 +69,84 @@ int main() {
 
 
 		case 6:
-			cout << "you are in the room 6. you can go east." << endl;
+			cout << "Make sure you get all the tools you'll need to get to build the skateboard has well." << endl;
 			cin >> input;
 			if (input == 'e ')
 				room = 6;
 			else
 				cout << "sorry, not an option." << endl;
 			break;
-		}
+		case 7:
+			cout << "you'll have 1400 to have every tool and the skateboard parts to the together ." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 7;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 8:
+			cout << " now your ready to start building the skateboard ." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 8;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 9:
+			cout << "first you get the board and the grip stape ." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 9;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 10:
+			cout << "measure the grip stape the cut it to match the board ." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 10;
+			else if (input == 'b')
+				room = 9;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 11:
+			cout << "then you start putting and scurrying the chucks on the board." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 11;
+			else if (input == 'b')
+				room = 13;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 12:
+			cout << "Now you can start putting the wheels on the board." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 12;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 13:
+			cout << "now you can put the bearings on the board ." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 13;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+		case 14:
+			cout << "now you are done with building the board and now you can buy the board to win the game ." << endl;
+			cin >> input;
+			if (input == 'e')
+				room = 6;
+			else if (input == 'b')
+				room = 13;
+			else
+				cout << "sorry, not an option." << endl;
+			break;
+
 	} while (input != 'q');
 			 
 } //function defintions------------------------
